140a4482efe27be015f26efde3a23b96ed745512
JhnRavelo <johnravelo135@gmail.com>
2024-07-04 15:09:58 +0300

    build: create fileHandler class

---
+++ class/FileHandler.js
@@ -0,0 +1,115 @@
+const fs = require("fs");
+const path = require("path");
+const sharp = require("sharp");
+
+class FileHandler {
+  constructor() {
+    const date = new Date();
+    this.dateArray = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
+  }
+
+  getDate() {
+    const date = new Date();
+    return `${
+      date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds()
+    }`;
+  }
+
+  createDirectory(dirPath, index) {
+    if (
+      fs.existsSync(path.join(dirPath, `${this.dateArray[index]}`)) &&
+      index < this.dateArray.length
+    ) {
+      return this.createDirectory(
+        path.join(dirPath, `${this.dateArray[index]}`),
+        index + 1
+      );
+    } else if (
+      !fs.existsSync(path.join(dirPath, `${this.dateArray[index]}`)) &&
+      index < this.dateArray.length
+    ) {
+      fs.mkdirSync(path.join(dirPath, `${this.dateArray[index]}`), {
+        recursive: true,
+      });
+      return this.createDirectory(
+        path.join(dirPath, `${this.dateArray[index]}`),
+        index + 1
+      );
+    } else {
+      return dirPath;
+    }
+  }
+
+  readDirectory(exportPath, filesArray = []) {
+    const files = fs.readdirSync(exportPath);
+    files.forEach((file) => {
+      const filePath = path.join(exportPath, file);
+      if (fs.statSync(filePath).isDirectory()) {
+        this.readDirectory(filePath, filesArray);
+      } else {
+        filesArray.push(filePath);
+      }
+    });
+    return filesArray;
+  }
+
+  createFile(fileName, data, ext, filePath, type) {
+    let name, fileDir, location;
+    const date = `-${this.getDate()}.`;
+    if (!fs.existsSync(filePath)) {
+      fs.mkdirSync(filePath);
+    }
+    if (type == "tmpApp") {
+      name = fileName + "." + ext;
+      fileDir = filePath;
+      location = path.join(filePath, name);
+    } else {
+      name = fileName + date + ext;
+      fileDir = this.createDirectory(filePath, 0);
+      if (type == "public") {
+        const publicPath = path.join(fileDir, name).split("public")[1];
+        location = `${process.env.SERVER_PATH}${publicPath.replace(
+          /\\/g,
+          "/"
+        )}`;
+      } else location = path.join(fileDir, name);
+    }
+    fs.writeFileSync(path.join(fileDir, name), data, {
+      encoding: "utf8",
+      flag: "w",
+    });
+    return { fileDir, location, date };
+  }
+
+  async createImage(req, imgPath) {
+    const galleryArray = new Array();
+    const response = req.files.map(async (file) => {
+      if (file.mimetype.split("/")[0] == "image") {
+        let webpData = await sharp(file.buffer).webp().toBuffer();
+        const { location } = this.createFile(
+          file.originalname.split(".")[0],
+          webpData,
+          "webp",
+          imgPath,
+          "public"
+        );
+        galleryArray.push(location);
+      }
+    });
+    await Promise.all(response);
+    return galleryArray.join(",");
+  }
+
+  deleteFileFromDatabase(deleted, dirPath, type) {
+    const location = deleted.split(type)[1];
+    let filePath;
+    if (type == "pdf") {
+      filePath = path.join(dirPath, location + "pdf");
+    } else filePath = path.join(dirPath, location);
+    if (fs.existsSync(filePath)) {
+      fs.unlinkSync(filePath);
+    }
+  }
+}
+
+module.exports = FileHandler;
